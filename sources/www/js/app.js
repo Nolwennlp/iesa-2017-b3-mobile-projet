d3376f1f2ebd2fd0adc38ac6897a2f094e82ded8
nmserain <n.serain@gmail.com>
2017-04-28 16:37:19 +0200

    spalsh

--- sources/www/js/app.js
+++ sources/www/js/app.js
@@ -7,129 +7,97 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-  .state('tab.userlist', {
-      url: '/userlist',
-      views: {
-        'tab-userlist': {
-          templateUrl: 'templates/tab-userlist.html',
-          controller: 'UserlistCtrl'
-        }
-      }
-    }).state('tab.ionicApp', {
-      url: '/ionicApp',
-      views: {
-        'tab-ionicApp': {
-          templateUrl: 'templates/tab-ionicApp.html',
-          controller: 'IonicAppCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-// geolocalisation
-    angular.module('ionicApp', ['ionic', 'ngMap'])
-        .controller('MarkerRemoveCtrl', function($scope, $ionicLoading) {
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-            $scope.positions = [{
-                lat: 43.07493,
-                lng: -89.381388
-            }];
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
 
-            $scope.$on('mapInitialized', function(event, map) {
-                $scope.map = map;
+        // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
+
+            // Each tab has its own nav history stack:
+
+            .state('tab.dash', {
+                url: '/dash',
+                views: {
+                    'tab-dash': {
+                        templateUrl: 'templates/tab-dash.html',
+                        controller: 'DashCtrl'
+                    }
+                }
+            })
+
+            .state('tab.chats', {
+                url: '/chats',
+                views: {
+                    'tab-chats': {
+                        templateUrl: 'templates/tab-chats.html',
+                        controller: 'ChatsCtrl'
+                    }
+                }
+            })
+            .state('tab.userlist', {
+                url: '/userlist',
+                views: {
+                    'tab-userlist': {
+                        templateUrl: 'templates/tab-userlist.html',
+                        controller: 'ContactCtrl'
+                    }
+                }
+            }).state('tab.ionicApp', {
+            url: '/ionicApp',
+            views: {
+                'tab-ionicApp': {
+                    templateUrl: 'templates/tab-ionicApp.html',
+                    controller: 'IonicAppCtrl'
+                }
+            }
+        })
+            .state('tab.chat-detail', {
+                url: '/chats/:chatId',
+                views: {
+                    'tab-chats': {
+                        templateUrl: 'templates/chat-detail.html',
+                        controller: 'ChatDetailCtrl'
+                    }
+                }
+            })
+
+            .state('tab.account', {
+                url: '/account',
+                views: {
+                    'tab-account': {
+                        templateUrl: 'templates/tab-account.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
             });
 
-            $scope.centerOnMe= function(){
-                $scope.positions = [];
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/dash');
 
-
-                $ionicLoading.show({
-                    template: 'Loading...'
-                });
-
-
-                navigator.geolocation.getCurrentPosition(function(position) {
-                    var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                    $scope.positions.push({lat: pos.k,lng: pos.B});
-                    console.log(pos);
-                    $scope.map.setCenter(pos);
-                    $ionicLoading.hide();
-                });
-
-            };
-
-        });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
+    });
 
--- sources/www/js/controllers.js
+++ sources/www/js/controllers.js
@@ -1,34 +1,72 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
-.controller('IonicAppCtrl', function($scope) {})
+    .controller('DashCtrl', function ($scope) {
+    })
+    .controller('IonicAppCtrl', function ($scope) {
+    })
 
 
-.controller('UserlistCtrl', function($scope) {
-  $scope.userlist= ['Ilyace','Nolwenn','NW','NB'];
-})
+    .controller('UserlistCtrl', function ($scope) {
+        $scope.userlist = ['Ilyace', 'Nolwenn', 'NW', 'NB'];
+    })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    .controller('ChatsCtrl', function ($scope, Chats) {
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+        $scope.chats = Chats.all();
+        $scope.remove = function (chat) {
+            Chats.remove(chat);
+        };
+    })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+    })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    .controller('AccountCtrl', function ($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
+    });
+
+module.controller('ContactCtrl', function ($scope, $cordovaContacts, $ionicPlatform) {
+    $scope.addContact = function () {
+        $cordovaContacts.save($scope.contactForm).then(function (result) {
+            // Contact saved
+        }, function (err) {
+            // Contact error
+        });
+    };
+
+    $scope.getAllContacts = function () {
+        $cordovaContacts.find().then(function (allContacts) { //omitting parameter to .find() causes all contacts to be returned
+            $scope.contacts = allContacts;
+        })
+    };
+
+
+    $scope.findContactsBySearchTerm = function (searchTerm) {
+        var opts = {                                           //search options
+            filter: searchTerm,                                 // 'Bob'
+            multiple: true,                                      // Yes, return any contact that matches criteria
+            fields: ['displayName', 'name'],                // These are the fields to search for 'bob'.
+            desiredFields: [id]    //return fields.
+    }
+
+        if ($ionicPlatform.isAndroid()) {
+            opts.hasPhoneNumber = true;         //hasPhoneNumber only works for android.
+        }
+
+        $cordovaContacts.find(opts).then(function (contactsFound) {
+            $scope.contacts = contactsFound;
+        })
+    }
+
+
+    });
