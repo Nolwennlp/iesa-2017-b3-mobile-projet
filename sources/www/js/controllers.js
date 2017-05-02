f6668d5dbde528b8ab3b3e2462edb61d19df5ffd
RedHotMan <ilyace.regaibi@gmail.com>
2017-05-02 10:48:06 +0200

    updates geoloc / sms

--- sources/www/js/controllers.js
+++ sources/www/js/controllers.js
@@ -50,12 +50,15 @@ angular.module('starter.controllers', [])
 
 
             }, function(err) {
-                // error
+                $ionicPopup.alert({
+                    title:'erreur',
+                    template: err
+                });
                 console.log(err);
             });
     })
 
-    .controller('InviteCtrl',function ($scope, $cordovaContacts, $cordovaSms, $ionicPopup, $cordovaStatusbar) {
+    .controller('InviteCtrl',function ($scope, $cordovaContacts, $cordovaSms, $ionicPopup, $cordovaStatusbar, $ionicPlatform) {
         // $cordovaContacts.find({multiple:true}).then(function(allContacts) {
         //         $scope.consoleContacts = JSON.stringify(allContacts);
         //         $scope.contacts = allContacts;
@@ -68,45 +71,26 @@ angular.module('starter.controllers', [])
             StatusBar.hide();
         }
 
-        var options = {
-            replaceLineBreaks: false, // true to replace \n by a new line, false by default
-            android: {
-                // intent: '' // send SMS with the native android SMS messaging
-                // intent: '' // send SMS without open any other app
-                intent: 'INTENT' // send SMS inside a default SMS app
-            }
-        };
-
         $scope.sendsms = function(){
-            var promptPopup = $ionicPopup.prompt({
-                title: 'Votre message',
-                template: 'Ecrivez un message',
-                inputType: 'text'
-            });
+            $ionicPlatform.ready(function () {
 
-            promptPopup.then(function(res) {
-                console.log(res);
-            }).then(function (res) {
-                $cordovaSms.send('0677293397',res).then(function(){
-                    console.log('bien vu');
-                })
+                var options = {
+                    replaceLineBreaks: false,
+                    android:{
+                        intent : 'INTENT',
+                        // intent : ''
+                    }
+                };
+                $cordovaSms.send('0677293397','This is a test sms', options).then(function(){
+                    alert("success! sms was sent");
+                },function(error){
+                    console.log(error);
+                    alert('Error sending sms!');
+                    console.log(error);
+
+                });
             })
-
-            // $cordovaSms.send('0677293397', 'Hello', options)
-            //     .then(function() {
-            //         // Success! SMS was sent
-            //         $ionicPopup.alert({
-            //             title:'succes',
-            //             template:'succes'
-            //         });
-            //     }, function(error) {
-            //         // An error occurred
-            //         $ionicPopup.alert({
-            //             title:'Fail',
-            //             template:'Fail'
-            //         });
-            //     });
-        }
+        };
     })
 
 .controller('ChatsCtrl', function($scope, Chats) {
